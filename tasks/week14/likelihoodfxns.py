4d931357f8d1bf707009dc9858469bd57a8c594b
lexiboone <alexandrakboone@gmail.com>
2025-04-21 15:22:26 -0600

    Added class 25 work to github

---
+++ tasks/week14/likelihoodfxns.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import chi2
+file = glob.glob('/d/scratch/ASTR5160/week13/line.data')[0]
+
+# Reading in file
+    
+df = pd.read_csv(file, delim_whitespace=True) # For tab-separated data
+#print(df)
+#print(df[0])
+# print(df.columns)
+
+x0 = np.array([df['#x']])
+# print(x0)
+x1 = np.array([df['0.5']])
+x2 = np.array([df['1.5']])
+x3 = np.array([df['2.5']])
+x4 = np.array([df['3.5']])
+x5 = np.array([df['4.5']])
+x6 = np.array([df['5.5']])
+x7 = np.array([df['6.5']])
+x8 = np.array([df['7.5']])
+x9 = np.array([df['8.5']])
+
+x_bins = [x0,x1,x2,x3,x4,x5,x6,x7,x8,x9]
+x_bins_names = ['x0','x1','x2','x3','x4','x5','x6','x7','x8','x9']
+O = []
+vara = []
+stda =[]
+# finding mean and variance and std
+for i in range(len(x_bins)):
+    mean = np.mean(x_bins[i])
+    var = np.var(x_bins[i], ddof = 1)
+    std = np.std(x_bins[i], ddof=1)
+    O.append(mean)
+    vara.append(var)
+    stda.append(std)
+    print(f'The mean for {x_bins_names[i]} is {mean}')
+    print(f'The variance for {x_bins_names[i]} is {var}')
+
+    
+print(vara)
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+import math
+# LB function to calculate ln posterior probability
+def compute_lnpp(m,b):
+    """Calculates ln posterior probability
+
+    Parameters
+    ----------
+    m,b: float (slope and intercept)
+    
+    Returns
+    -------
+    ln posterior probability for the function given in '/d/scratch/ASTR5160/week13/line.data'
+    given some m and b. 
+    
+    Notes
+    -------
+    Will return -np.inf for b less than 0 or larger than 8 (values are outside of m,b space we 
+    want to sample)
+    """
+    x = np.arange(0.5,10,1)
+    yact = O
+    E = x*m + b
+    v = np.array(vara)
+    chi_sq = ((O-E)**2)/v 
+    st = 2*math.pi*v
+    secondterm = np.log(st)
+    if 0<b<8:
+        return -0.5*np.sum(chi_sq+secondterm) + np.log(1)
+    else:
+        return -np.inf
+    #print(y)
+    
+compute_lnpp(3,4)
+
+def MH_MCMCchain(m,b):
+    """Calculates R and returns new values of m and b according to gaussian proposal function
+
+    Parameters
+    ----------
+    m,b: float (slope and intercept as priors)
+    
+    Returns
+    -------
+    ln posterior probability - ln new posterior probability for the function given 
+    in '/d/scratch/ASTR5160/week13/line.data' given some m and b and a step size. 
+    
+    Notes
+    -------
+    Will tell you when you should accept new parameters based on if R >1
+    """
+    delta_m = 0.1
+    delta_b = 0.1
+    # LB gaussian propoasl function with steps defined above
+    newm = np.random.normal(m,delta_m)
+    newb = np.random.normal(b,delta_b)
+    lnR = compute_lnpp(m,b) - compute_lnpp(newm,newb)
+    if np.exp(lnR) > 1:
+        print('Always accept new parameters')
+        return np.exp(lnR), newm, newb
+        
+    else:
+        print(f'Accept new parameters with probability {np.exp(lnR)}')
+        return np.exp(lnR), newm, newb
+        
+
+
+MH_MCMCchain(3,4)
+
+
+# In[ ]:
+
+
+
+
